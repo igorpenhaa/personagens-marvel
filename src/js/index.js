3e12cdbe1b693e3a02c2be41cf3c94827420989b
igorpenhaa <98900627+igorpenhaa@users.noreply.github.com>
2022-05-18 15:55:27 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+const personagens = window.document.querySelectorAll('.personagem')
+
+personagens.forEach((personagem) => {
+  personagem.addEventListener('mouseenter', () => {
+    const personagemSelecionado = window.document.querySelector('.selecionado')
+    personagemSelecionado.classList.remove('selecionado')
+
+
+    personagem.classList.add('selecionado')
+
+    const idSelecionado = personagem.attributes.id.value
+
+    const imagemJogador1 = window.document.getElementById('personagem-jogador-1')
+    imagemJogador1.src = `./src/imagens/${idSelecionado}.png`
+
+    const nomeJogador1 = document.getElementById('nome-jogador-1')
+    const nomeSelecionado = personagem.getAttribute('data-name')
+
+    nomeJogador1.innerHTML = `${nomeSelecionado}`
+
+  })
+})
